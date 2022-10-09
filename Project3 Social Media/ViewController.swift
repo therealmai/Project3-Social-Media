ffb79e4bcaa48eb8b2dc1c5ee835fcbe6faa4892
Jomar Leano <jomar35leano@gmail.com>
2022-10-09 21:47:42 +0800

    Initial Commit

---
+++ Project3 Social Media/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project3 Social Media
+//
+//  Created by Jomar Leano on 10/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
